c4851ac15b763fefdfe72d62c1b41cc9be3ba2e6
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-21 10:42:44 -0500

    initial commit

---
+++ src/main/java/com/codeup/adlister/util/Password.java
@@ -0,0 +1,27 @@
+
+
+package com.codeup.adlister.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class codeRunner {
+
+    public static void main(String[] args) {
+
+        String password = "supersecretpw";
+
+        String hashPW8 = BCrypt.hashpw(password, BCrypt.gensalt(8));
+        String hashPW10 = BCrypt.hashpw(password, BCrypt.gensalt(10));
+        String hashPW12 = BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+        System.out.println(password + " was our original password");
+        System.out.println(hashPW8 + ": (8 rounds) is the result after a little bcrypt :o");
+        System.out.println(hashPW10 + ": (10 rounds) is the result after a little bcrypt :o");
+        System.out.println(hashPW12 + ": (12 rounds) is the result after a little bcrypt :o");
+
+        System.out.println();
+
+        System.out.println("BCrypt.checkpw(\"notsosecretpw\", hashPW12) = " + BCrypt.checkpw("notsosecretpw", hashPW12));
+
+    }
+}
