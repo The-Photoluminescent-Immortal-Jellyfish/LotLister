a5a12025144c78d3211756540fe12b7bc8bee29f
Stout13 <stout.stephen13@gmail.com>
2022-06-24 16:38:54 -0500

    Lots of pseudo-code planning for DAO to CRUD operations

---
+++ src/main/java/backend/controllers/CrudAdServlet.java
@@ -0,0 +1,12 @@
+package backend.controllers;
+
+//      make a servlet that receives CRUD requests from various forms,
+//      runs associated subprograms,
+//      to interact with DB,
+//      and show verification of success or failure
+//      and redirect url to appropriate page
+//      decouple methods to create new classes when necessary
+
+public class CrudAdServlet {
+
+}
