b5fe0175972c28027f6a41c9010b371820980b38
Stout13 <stout.stephen13@gmail.com>
2022-06-27 23:45:10 -0500

    almost have insert for MySqlUsersDao and MySqlLotsDao

--- src/main/java/backend/models/Lots.java
+++ src/main/java/backend/models/Lots.java
@@ -1,14 +1,20 @@
 package backend.models;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Lots {
     User findByTitle(String title);
-
-    Long insert(String username, String email, String password);
-
     // get a list of all the ads
     List<Lot> all();
     // insert a new ad and return the new ad's id
-    int insert(Lot ad);
+
+    static Lot insert(String Title, String Description, String Street_number,
+                      String Street_name, String Username, String Email,
+                      String Appraisal, String Sale_price, String City,
+                      String State, String Gov_org,
+                      Connection Connection) throws SQLException {
+        return null;
+    }
 }
